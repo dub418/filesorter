d510fc85ab5be3dd1b8a47698f7f021bd175fe07
Yu <Yury@HPY>
2015-09-12 07:31:50 +0300

    new

--- src/com/resanc/filesorter/FSMetrics.java
+++ src/com/resanc/filesorter/FSMetrics.java
@@ -8,5 +8,14 @@ package com.resanc.filesorter;
  *
  */
 public class FSMetrics {
+	private long previousTime=0;
+	private long currentTime=0;
+	
+	public FSMetrics()
+	{
+		currentTime=System.currentTimeMillis();
+	}
+	
+	
 
 }
